440c198a079bd4a72e44b90e7585a0f0bc5870b5
NikitaLipatov <Ovenvan@yandex.ru>
2017-06-21 16:19:44 +0300

    Новая система генерации названий фотографий

--- src/main/java/topprogersgroup/service/Impl/CurrentUserServiceImpl.java
+++ src/main/java/topprogersgroup/service/Impl/CurrentUserServiceImpl.java
@@ -4,10 +4,15 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Service;
+import topprogersgroup.entity.Bid;
 import topprogersgroup.entity.CurrentUser;
+import topprogersgroup.entity.Pet;
 import topprogersgroup.entity.Role;
 import topprogersgroup.service.BidService;
 import topprogersgroup.service.CurrentUserService;
+import topprogersgroup.service.PetService;
+
+import java.util.List;
 
 @Service
 public class CurrentUserServiceImpl implements CurrentUserService {
@@ -15,9 +20,26 @@ public class CurrentUserServiceImpl implements CurrentUserService {
     @Autowired
     private BidService bidService;
 
+    @Autowired
+    private PetService petService;
+
     public boolean canAccessOwnerBids(CurrentUser currentUser, int bidId) {
         return currentUser != null
-                && (currentUser.getUser().getOwner().getPet().contains(bidService.findOne(bidId)));
+                && (checkContainsBid(currentUser, bidId));
+    }
+
+    public boolean checkContainsBid(CurrentUser currentUser, int bidId) {
+        List<Pet> pets = currentUser.getUser().getOwner().getPet();
+        Bid bid = bidService.findOne(bidId);
+        for (Pet pet : pets) {
+            if (pet.getBids().contains(bid)) return true;
+        }
+        return false;
+    }
+
+    public boolean canAccessOwnerPets(CurrentUser currentUser, int petId) {
+        return currentUser != null
+                && (currentUser.getUser().getOwner().getPet().contains(petService.findOne(petId)));
     }
 
     @Override
